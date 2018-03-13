fcdb38dfe4f82421bbf26a853a04cfbcde1b06ff
clei1 <clei1@stuy.edu>
2018-03-13 16:31:44 -0400

    fancy donut

---
+++ index.js
@@ -0,0 +1,47 @@
+//hardcoded medal values
+var us = [{"cx":50, "cy":50, "r":9, "color":"brown"},
+	  {"cx":200, "cy":200, "r":8, "color":"silver"},
+	  {"cx":250, "cy":250, "r":6, "color":"yellow"}];
+var can = [{"cx":50, "cy":50, "r":11, "color":"brown"},
+	   {"cx":200, "cy":200, "r":8, "color":"silver"},
+	   {"cx":250, "cy":250, "r":10, "color":"yellow"}];
+var st = true; //US MEDALS
+
+//add svg
+var svg = d3.select("body").append("svg")
+    .attr("width", 500)
+    .attr("height", 500);
+
+//add circle elements
+var circles = svg.selectAll("circle")
+    .data(us)
+    .enter()
+    .append("circle");
+
+var addAttr = function(){
+    circles.attr("cx", function(d){return d.cx} )
+	.attr("cy", function(d){return d.cy} )
+	.attr("r", function(d){return d.r} )
+	.style("fill", function(d){return d.color});
+}
+
+var update = function(country){
+    circles = svg.selectAll("circle")
+	.data(country);
+    addAttr();
+};
+
+var button = d3.select("body").append("button")
+    .text(function(){
+	return "Show me US's medals!";
+    })
+    .on("click", function(){
+	if(st)
+	{ update(us);
+	  button.text("Show me Canda's medals!");}
+	else
+	{ update(can);
+	  button.text("Show me US's medals!");}
+	st = !st;
+    });
+
